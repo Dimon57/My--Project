7217503afd4482c6f6528255128c62c49cb8fb1c
Dimon57 <cernenkodmitrij57@gmail.com>
2023-04-24 17:30:07 +0300

    added negative tests for button

--- cypress/e2e/spec/elements/buttons.cy.js
+++ cypress/e2e/spec/elements/buttons.cy.js
@@ -1,6 +1,7 @@
 const DOUBLE_CLICK = '#doubleClickBtn'
 const RIGHT_CLICK = '#rightClickBtn'
 const CLICK_ME = '[type="button"]'
+
 const DOUBLE_CLICK_BOARDER = '#doubleClickMessage'
 const RIGHT_CLICK_BOARDER = '#rightClickMessage'
 const CLICK_ME_BOARDER = '#dynamicClickMessage'
@@ -12,40 +13,40 @@ describe ('Buttons',()=>{
         
     })
 
-    it.skip('Click Btn',()=>{
+    it('Click Btn',()=>{
        cy.get(DOUBLE_CLICK).dblclick()
        cy.get(RIGHT_CLICK).rightclick()
        cy.get(CLICK_ME).eq(3).click()
     })
-    it.skip('You have done a double click',()=>{
+    it('You have done a double click',()=>{
         cy.get(DOUBLE_CLICK).dblclick()
         cy.get(RIGHT_CLICK).rightclick()
         cy.get(CLICK_ME).eq(3).click()
         
         cy.get(DOUBLE_CLICK_BOARDER).should('have.text','You have done a double click')
     })
-    it.skip('You have done a right click',()=>{
+    it('You have done a right click',()=>{
         cy.get(DOUBLE_CLICK).dblclick()
         cy.get(RIGHT_CLICK).rightclick()
         cy.get(CLICK_ME).eq(3).click()
         
         cy.get(RIGHT_CLICK_BOARDER).should('have.text','You have done a right click')
     })
-    it.skip('You have done a dynamic click',()=>{
+    it('You have done a dynamic click',()=>{
         cy.get(DOUBLE_CLICK).dblclick()
         cy.get(RIGHT_CLICK).rightclick()
         cy.get(CLICK_ME).eq(3).click()
         
         cy.get(CLICK_ME_BOARDER).should('have.text','You have done a dynamic click')
     })
-    it.skip('One click on double click',()=>{
+    it('One click on double click',()=>{
         cy.get(DOUBLE_CLICK).click()
         cy.get(RIGHT_CLICK).rightclick()
         cy.get(CLICK_ME).eq(3).click()
 
         cy.get(DOUBLE_CLICK_BOARDER).should('not.exist')
     })
-    it.skip('Left click on right click',()=>{
+    it('Left click on right click',()=>{
         cy.get(DOUBLE_CLICK).dblclick()
         cy.get(RIGHT_CLICK).click()
         cy.get(CLICK_ME).eq(3).click()
